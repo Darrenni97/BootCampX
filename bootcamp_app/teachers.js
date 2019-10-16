6f4d2f64e6ae246dff6454a7d38b40ff00afffcf
Darrenni97 <Darrenni_@hotmail.com>
2019-10-16 16:27:58 -0700

    fixed for sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,16 +9,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${args[0]}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`
+const values = [args[0]]
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
