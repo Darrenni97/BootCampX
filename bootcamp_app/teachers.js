22d832ad0febf8756e4ce69348cfd5cf99b3bb08
Darrenni97 <Darrenni_@hotmail.com>
2019-10-16 15:18:46 -0700

    query for teachers in a cohort

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+const args = process.argv.slice(2)
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${args[0]}'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
